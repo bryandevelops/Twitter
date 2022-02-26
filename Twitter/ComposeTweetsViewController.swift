0814b7c09a052a904c8761a2f42117c6e29acb13
bryan0147 <bryan0147@gmail.com>
2022-02-26 15:53:55 -0500

    Add placeholder text for the tweet Text View. Add alert message for posting a tweet with no text. Change the look of the tweet Text View.

--- Twitter/ComposeTweetsViewController.swift
+++ Twitter/ComposeTweetsViewController.swift
@@ -8,26 +8,89 @@
 
 import UIKit
 
-class ComposeTweetsViewController: UIViewController {
+class ComposeTweetsViewController: UIViewController, UITextViewDelegate {
 
-    @IBOutlet weak var tweetTextView: UITextView!
+    @IBOutlet weak var textView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tweetTextView.becomeFirstResponder()
+        
+        textView.layer.borderWidth = 1
+        textView.layer.borderColor = UIColor.gray.cgColor
+        textView.layer.cornerRadius = 25
+        textView.contentInset = UIEdgeInsets(top: 0, left: 12, bottom: 0, right: 12)
+        
+        textView.text = "What's happening?"
+        textView.textColor = UIColor.lightGray
+        textView.becomeFirstResponder()
+        textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+
     }
+    // Show the placeholder text whenever the text view is empty, even if the text view’s selected
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+
+        // Combine the textView text and the replacement text to
+        // create the updated text string
+        let currentText:String = textView.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+
+        // If updated text view will be empty, add the placeholder
+        // and set the cursor to the beginning of the text view
+        if updatedText.isEmpty {
+
+            textView.text = "What's happening?"
+            textView.textColor = UIColor.lightGray
+
+            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+        }
+
+        // Else if the text view's placeholder is showing and the
+        // length of the replacement string is greater than 0, set
+        // the text color to black then set its text to the
+        // replacement string
+         else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+            textView.textColor = UIColor.black
+            textView.text = text
+        }
+
+        // For every other case, the text should change with the usual
+        // behavior...
+        else {
+            return true
+        }
+
+        // ...otherwise return false since the updates have already
+        // been made
+        return false
+    }
+
+    // Prevent the user from changing the position of the cursor while the placeholder's visible
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if self.view.window != nil {
+            if textView.textColor == UIColor.lightGray {
+                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            }
+        }
+    }
+
     
     @IBAction func onCancel(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func onTweet(_ sender: Any) {
-        if (!tweetTextView.text.isEmpty) {
-            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+        // Create an alert that will be used if the tweet is empty and can't be posted
+        let alertController = UIAlertController(title: "Alert", message: "Your tweet is empty.", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(OKAction)
+        
+        if (!textView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: textView.text, success: {
                 self.dismiss(animated: true, completion: nil)
             }, failure: { Error in
                 print("Could not post tweet.")
                 print(Error.localizedDescription)
+                self.present(alertController, animated: true, completion: nil) // Call the alert
             })
         }
     }
